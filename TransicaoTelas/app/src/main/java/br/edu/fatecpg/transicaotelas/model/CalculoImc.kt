41a630bdd64fe8063f90f89c035705b16bf83377
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2024-08-27 20:09:35 -0300

    Kotlin - IMC

---
+++ TransicaoTelas/app/src/main/java/br/edu/fatecpg/transicaotelas/model/CalculoImc.kt
@@ -0,0 +1,9 @@
+package br.edu.fatecpg.transicaotelas.model
+
+class CalculoImc {
+
+    fun calcula(peso: Double, altura: Double): Double{
+        val imc = (peso / (altura * altura))
+        return imc
+    }
+}
---
+++ TransicaoTelas/app/src/main/java/br/edu/fatecpg/transicaotelas/model/Usuario.kt
@@ -0,0 +1,7 @@
+package br.edu.fatecpg.transicaotelas.model
+
+data class Usuario(
+    val peso:Double = 0.0,
+    val altura:Double = 0.0,
+    val imc:Double = 0.0
+)
